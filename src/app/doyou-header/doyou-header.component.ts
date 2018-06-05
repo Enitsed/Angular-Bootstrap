eb472901a524c3ab3f801c6074055d2aaab87585
enitsed <sllki1@naver.com>
2018-06-05 17:29:04 +0900

    Add Modal for login and join

--- src/app/doyou-header/doyou-header.component.ts
+++ src/app/doyou-header/doyou-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-doyou-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoyouHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+  }
+
+  openVerticallyCentered(content) {
+    this.modalService.open(content, { centered: true });
   }
 
 }
